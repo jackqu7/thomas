9b0834f7bb9cf4d59ebbbc566c953b3d29bbc610
Jack Saunders <jack@oldstlabs.com>
2015-10-21 19:08:48 +0100

    ability to draw fringe trains in different colour

--- thomas/drawer.py
+++ thomas/drawer.py
@@ -11,11 +11,13 @@ class Drawer(object):
     large_font = ImageFont.truetype(get_asset_filename('font.ttf'), 54)
     small_font = ImageFont.truetype(get_asset_filename('font.ttf'), 20)
 
-    text_color = (0, 204, 255)
+    BLUE = (0, 204, 255)
+    ORANGE = (255, 153, 51)
 
-    def __init__(self, train):
+    def __init__(self, train, text_color=BLUE):
         self.im = Image.new('RGB', (self.WIDTH, self.HEIGHT))
         self.train = train
+        self.text_color = text_color
 
     def draw(self):
         return self.blank()
@@ -23,7 +25,7 @@ class Drawer(object):
     def blank(self):
         return self.im
 
-    def draw_text(self, draw, coords, text, font, center=True):
+    def draw_text(self, draw, coords, text, font, color, center=True):
         x, y = coords
 
         if center:
@@ -33,23 +35,28 @@ class Drawer(object):
 
             y = y - h / 2
 
-        draw.text((x, y), text, font=font, fill=self.text_color)
+        draw.text((x, y), text, font=font, fill=color)
 
 
 class HeadcodeDrawer(Drawer):
     def draw(self):
         if self.train:
-            return self.headcode(self.train['headcode'])
+            if self.train.get('is_fringe'):
+                color = self.ORANGE
+            else:
+                color = self.BLUE
+            return self.headcode(self.train['headcode'], color=color)
         else:
             return self.blank()
 
-    def headcode(self, headcode):
+    def headcode(self, headcode, color=Drawer.BLUE):
         draw = ImageDraw.Draw(self.im)
 
         self.draw_text(
             draw,
             (self.WIDTH / 2 + 4, self.HEIGHT / 2),
             headcode,
-            self.large_font)
+            self.large_font,
+            color)
 
         return self.im
