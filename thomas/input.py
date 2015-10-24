6efe7bd06d0c4ab10570be87045663327e7c6dee
Jack Saunders <jack@oldstlabs.com>
2015-10-24 14:43:29 +0100

    take input from stdin

--- thomas/input.py
+++ thomas/input.py
@@ -1,6 +1,7 @@
 import os
 import asyncio
 import multiprocessing
+import sys
 from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
 
 from .util import get_asset_filename
@@ -24,10 +25,10 @@ if has_lirc:
 
         def get_next_code(self, q):
             loop = asyncio.get_event_loop()
-            return (yield from loop.run_in_executor(ThreadPoolExecutor(max_workers=1),
+            return (yield from loop.run_in_executor(
+                ThreadPoolExecutor(max_workers=1),
                     q.get))
 
-        @asyncio.coroutine
         def run(self, message_queue):
             m = multiprocessing.Manager()
             q = m.Queue()
@@ -35,31 +36,38 @@ if has_lirc:
             loop = asyncio.get_event_loop()
             loop.run_in_executor(ProcessPoolExecutor(max_workers=1),
                                  lirc_process, q)
+            asyncio.async(self._run(q, message_queue))
+
+        @asyncio.coroutine
+        def _run(self, q, message_queue):
             while True:
                 result = yield from self.get_next_code(q)
                 for code in result:
                     message_queue.put_nowait(code)
 
 else:
-    class InputDummy(object):
-        @asyncio.coroutine
+    class InputStdin(object):
+        def got_stdin_data(self, message_queue):
+            asyncio.async(message_queue.put(sys.stdin.readline()))
+
         def run(self, message_queue):
-            yield
+            loop = asyncio.get_event_loop()
+            loop.add_reader(sys.stdin, self.got_stdin_data, message_queue)
 
 
 class Input(object):
     def __init__(self):
         is_arm = os.uname()[4][:3] == 'arm'
-        self.input = InputLIRC() if is_arm else InputDummy()
+        self.input = InputLIRC() if is_arm else InputStdin()
         self.callbacks = {}
 
     @asyncio.coroutine
     def run(self):
         message_queue = asyncio.Queue()
-        asyncio.async(self.input.run(message_queue))
+        self.input.run(message_queue)
         while True:
             result = yield from message_queue.get()
-            print(result)
+            print('Got input %s' % result)
             if result in self.callbacks:
                 self.callbacks[result]()
 
